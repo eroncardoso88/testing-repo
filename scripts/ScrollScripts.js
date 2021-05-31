90fc60848d6fa21fc842e79cb0c5430f03572643
eron <eron@eron.eron>
2021-05-31 15:21:53 -0300

    tesdt

---
+++ scripts/ScrollScripts.js
@@ -0,0 +1,69 @@
+(function () {
+
+    var whatToScroll = $('main')
+    $("#butOne").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#one").offset().top
+        }, 2000);
+    });
+    $("#butTwo").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#two").offset().top
+        }, 2000);
+    });
+    $("#butThree").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#three").offset().top
+        }, 2000);
+    });
+    $("#butFour").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#four").offset().top
+        }, 2000);
+    });
+    $("#butFive").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#five").offset().top
+        }, 2000);
+    });
+    $("#butSix").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#six").offset().top
+        }, 2000);
+    });
+    $("#butSeven").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#seven").offset().top
+        }, 2000);
+    });
+    $("#butEight").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#eight").offset().top
+        }, 2000);
+    });
+    $("#butNine").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#nine").offset().top
+        }, 2000);
+    });
+    $("#butTen").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#ten").offset().top
+        }, 2000);
+    });
+    $("#butEleven").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#eleven").offset().top
+        }, 2000);
+    });
+    $("#butTwelve").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#twelve").offset().top
+        }, 2000);
+    });
+    $("#butThirteen").click(function() {
+        $(whatToScroll).animate({
+            scrollTop: $("#thirteen").offset().top
+        }, 2000);
+    });
+})();
